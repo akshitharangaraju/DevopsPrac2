fc4dc6eea780e4e0327da7baa1163cc8b20edaad
akshitha rangaraju <akshitharangaraju@gmail.com>
2026-01-08 15:15:02 +0530

    Update HelloDevOpsTest.java

--- src/test/java/com/example/HelloDevOpsTest.java
+++ src/test/java/com/example/HelloDevOpsTest.java
@@ -2,7 +2,7 @@ package com.example;
 public class HelloDevOpsTest { 
     public static void main(String[] args) { 
         System.out.println("Testing HelloDevOps class..."); 
-        String message = HelloDevOps.getMessage(); 
+        String message = com.example.HelloDevOps.getMessage(); 
         System.out.println("Message: " + message); 
         if (message.equals("Hello from DevOps Training")) { 
             System.out.println(" Test passed!"); 
